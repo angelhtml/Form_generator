3256f7c2392c10994771120da155f811ebbe11b3
Angel <82230757+angelhtml@users.noreply.github.com>
2022-12-12 19:22:17 +0330

    Update Headeruser.jsx

--- formTask/Headeruser.jsx
+++ formTask/Headeruser.jsx
@@ -1,6 +1,6 @@
 import HeaderStyle from './Headeruser.module.css';
+
 export default function Headeruser(title){
-    console.log(title)
     return(
         <div className={HeaderStyle.header}>
             <h1>{title.title}</h1>
