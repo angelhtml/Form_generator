d5428a4835a7a4b1aa43bccc73d23795567f953d
Angel <82230757+angelhtml@users.noreply.github.com>
2023-01-19 22:18:15 +0330

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,11 +9,11 @@ function App(){
         <title>Fullstack Engineer Tech Test</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="manifest" href='/manifest.json' />
-    
       </Head>
       <Header />
       <Form />
     </div>
   )
 }
-export default App
+
+export default App;
